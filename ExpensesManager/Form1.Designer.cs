e73a21e1a9c600c8cabd24f17d76905b22785921
adam abdellaoui <adamsuccess2001@gmail.com>
2021-03-11 17:21:30 +0100

    Add project files.

---
+++ ExpensesManager/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace ExpensesManager
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(63, 52);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(318, 26);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "C:\\Users\\ADAM\\Desktop\\expenses.txt";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(407, 52);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(78, 36);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Read";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(63, 146);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(475, 253);
+            this.textBox2.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ ExpensesManager/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ExpensesManager
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private IEnumerable<string> ReadFile()
+        {
+            var path = textBox1.Text;
+
+            if (!File.Exists(path))
+            {
+                MessageBox.Show("File does not exist. Cannot proceed");
+                return null;
+            }
+
+            var lines = File.ReadAllLines(path).Skip(1);
+            return lines;
+        }
+
+        private List<Expense> ListOfExpenses()
+        {
+            var lines = ReadFile();
+
+            var expenses = new List<Expense>();
+            foreach (var line in lines)
+            {
+                var split = line.Split('|');
+                var date = DateTime.ParseExact(split[0], "yyyy-MM-dd", null);
+                var price = Convert.ToDecimal(split[1].Replace(".", CultureInfo.CurrentCulture.NumberFormat.NumberDecimalSeparator));
+                var category = split[2];
+
+
+
+                var expense = new Expense(date, price, category);
+                expenses.Add(expense);
+            }
+            return expenses;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var result = ListOfExpenses();
+            var numbOfCateg = result
+                .GroupBy(x => x.Category)
+                .Count();
+            var totalExpenses = result
+                .Sum(x => x.Price);
+
+            textBox2.Text = $"Number of categories: {numbOfCateg}, total expenses: {totalExpenses}";
+
+            textBox2.Text +=
+                $"\r\nCategories: {string.Join(", ", result.GroupBy(x => x.Category).Select(x => x.Key))}";
+            textBox2.Text +=
+                $"\r\nDates: {string.Join(", ", result.GroupBy(x => x.Date.ToString("dd/MM/yyyy")).Select(x => x.Key))}";
+
+            
+        }
+    }
+
+    public class Expense
+    {
+        public DateTime Date { get; }
+        public decimal Price { get; }
+        public string Category { get; }
+
+        public Expense(DateTime date, decimal price, string categ)
+        {
+            Date = date;
+            Price = price;
+            Category = categ;
+        }
+    }
+}
