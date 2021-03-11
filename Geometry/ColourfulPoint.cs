e2f9be37a4f0cd600270e3c3cb16dcbe24d5f18d
adam abdellaoui <adamsuccess2001@gmail.com>
2021-03-11 18:14:48 +0100

    2nd part

---
+++ Geometry/ColourfulPoint.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Geometry
+{
+    public enum PointColour
+    {
+        Red,
+        Green,
+        Blue
+    }
+    class ColourfulPoint: Point
+    {
+        public PointColour Colour { get; set; }
+
+        public ColourfulPoint(double x, PointColour col) : base(x)
+        {
+            Colour = col;
+        }
+
+        public ColourfulPoint(double x, double y, PointColour col) : base(x, y)
+        {
+            Colour = col;
+        }
+    }
+}
---
+++ Geometry/Point.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Geometry
+{
+    public interface IReflectable
+    {
+        public void ReflectX();
+        public void ReflectY();
+    }
+    public class Point : IReflectable
+    {
+        public double X
+        {
+            get
+            {
+                return X;
+            }
+
+            private set
+            {
+                X = value;
+            }
+        }
+
+        public double Y
+        {
+            get
+            {
+                return Y;
+            }
+
+            private set
+            {
+                Y = value;
+            }
+        }
+
+        public Point(double x)
+        {
+            X = x;
+            Y = 0;
+        }
+
+        public Point(double x, double y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        void IReflectable.ReflectX()
+        {
+            Y = Y * -1;
+        }
+
+        void IReflectable.ReflectY()
+        {
+            X = X * -1;
+        }
+    }
+}
